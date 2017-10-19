2fd948e50176d3fa4c37148a7cdc4b2c358bc40e
badi-zixdev <badi1wow@gmail.com>
2017-10-19 10:10:01 +0100

    added: assets, routes provider

---
+++ Providers/AssetsServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Zix\Core\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class AssetsServiceProvider
+ * @package Zix\Core\Providers
+ */
+class AssetsServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerViews();
+    }
+
+
+    /**
+     * Register Core Views
+     */
+    public function registerViews()
+    {
+        $viewPath = base_path('views/modules/core');
+
+        $sourcePath = __DIR__ . '/../Assets/views';
+
+        $this->publishes([
+            $sourcePath => $viewPath
+        ]);
+
+        $this->loadViewsFrom([$viewPath, $sourcePath], 'core');
+    }
+
+}
