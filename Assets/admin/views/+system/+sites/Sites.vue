9be4a876ee6f428660b88dbb00ca53b991771f4f
badi ifaoui <badi0wow@gmail.com>
2018-01-07 13:12:14 +0100

    updated: sites table actions

--- Assets/admin/views/+system/+sites/Sites.vue
+++ Assets/admin/views/+system/+sites/Sites.vue
@@ -2,6 +2,7 @@
     <div>
         <data-table
                 :headers="headers"
+                :actions="actions"
                 api-route="sites"
                 route="system.sites"
         ></data-table>
@@ -20,38 +21,60 @@
         }
     })
     export default class Sites extends Vue {
-        headers = [
-            {
-                text: 'table.name',
-                value: 'name',
-                align: 'left',
-                searchable: true,
-                sortable: true,
-            },
-            {
-                text: 'table.url',
-                value: 'url',
 
-                searchable: true,
-                sortable: true,
-            },
-            {
-                text: 'table.ui',
-                value: 'ui',
-                searchable: true,
-                sortable: true,
-            },
-            {
-                text: 'table.status',
-                value: 'status',
-                sortable: true,
-            },
-            {
-                text: 'table.actions',
-                value: 'actions',
-                align: 'right',
-            }
-        ];
+        created() {
+            this.actions = [
+                {
+                    text: 'table.edit',
+                    color: 'primary',
+                    fab: true,
+                    icon: 'edit',
+                    callback: (item) => this.$router.push({name: 'system.sites.edit', params: {id: item.id}})
+                },
+                {
+                    text: 'table.delete',
+                    color: 'red',
+                    dark: true,
+                    fab: true,
+                    icon: 'delete',
+                    callback: (item) => this.$events.$emit('deleteData', item.id)
+                }
+            ];
+
+            this.headers = [
+                {
+                    text: 'table.name',
+                    value: 'name',
+                    align: 'left',
+                    searchable: true,
+                    sortable: true,
+                },
+                {
+                    text: 'table.url',
+                    value: 'url',
+
+                    searchable: true,
+                    sortable: true,
+                },
+                {
+                    text: 'table.ui',
+                    value: 'ui',
+                    searchable: true,
+                    sortable: true,
+                },
+                {
+                    text: 'table.status',
+                    value: 'status',
+                    sortable: true,
+                },
+                {
+                    text: 'table.actions',
+                    value: 'actions',
+                    align: 'right',
+                }
+            ];
+        }
+
 
         mounted() {
             this.$store.dispatch('setBreadCrumbs', [
